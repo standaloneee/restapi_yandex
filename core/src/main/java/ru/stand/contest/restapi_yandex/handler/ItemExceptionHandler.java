5ac452ece51aa747ea5ec39629494a30cab09378
gtambovtsev <gtambovtsev@inpolus.ru>
2022-09-12 19:39:39 +0300

    add deletion validation

--- core/src/main/java/ru/stand/contest/restapi_yandex/handler/ItemExceptionHandler.java
+++ core/src/main/java/ru/stand/contest/restapi_yandex/handler/ItemExceptionHandler.java
@@ -1,6 +1,5 @@
 package ru.stand.contest.restapi_yandex.handler;
 
-import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -14,8 +13,17 @@ import javax.validation.ConstraintViolationException;
 public class ItemExceptionHandler {
 
 
-    @ExceptionHandler({ValidationItemException.class, HttpMessageNotReadableException.class, ConstraintViolationException.class, InvalidFormatException.class})
+    @ExceptionHandler({
+            ValidationItemException.class,
+            HttpMessageNotReadableException.class,
+            ConstraintViolationException.class,
+            ItemNotFoundException.class})
     protected ResponseEntity<Error> handleItemException(final RuntimeException ex) {
+
+        if (ex instanceof ItemNotFoundException) {
+            return new ResponseEntity<>(new Error().code(404).message(ex.getMessage()), HttpStatus.NOT_FOUND);
+        }
+
         Error error = new Error();
         error.code(400).message("Validation Failed");
 //        error.code(400).message(ex.getMessage());
